cc0f726649e9b608d84bbb988688360f021dc578
priyanka <priyanka.chavan1408@gmail.com>
2022-04-04 17:48:10 +0530

    [Priyanka]Added:Calculated employee wage based on their shift using switch case statement.

--- EmployeeWageBuilder.py
+++ EmployeeWageBuilder.py
@@ -1,31 +1,84 @@
 '''
 @Author : Priyanka
-@Date : 2022-04-02  17:15:00
+@Date : 2022-04-02  17:25:00
 @Last Modified by : Priyanka
-@Last Modified Time : 2022-04-02  17:20:00
-@Title : Calculate employee wage based on their shift.
+@Last Modified Time : 2022-04-02  17:35:00
+@Title : Calculate employee wage based on their shift using switch case statement.
 '''
 
 import random
 
 print("Welcome to the employee wage computation program")
 
-EMP_RATE_PER_HOUR = 20
 fullDayEmpHours = 8
 halfDayEmpHours = 4
-empHours = 0
-empWage = 0
+EMP_RATE_PER_HOUR = 20
 
-empCheck = random.randint(0,1)
 
-if empCheck == 1:
-    print("Employee is present")
-    dayCheck = random.randint(0,1)
-    if dayCheck == 0:
-        empWage = EMP_RATE_PER_HOUR * fullDayEmpHours
-        print("Employee is full day present and employee wage for full day is",empWage)
-    else:
-        empWage = EMP_RATE_PER_HOUR * halfDayEmpHours
-        print("Employee is half day present and employee wage is", empWage)
+def empFullDayWage():
+    """
+        Description:
+            Calculating employee full day wage
+        Parameter:
+            None
+        Return:
+            Total employee wage and presenty message.
+    """
+    empWage = fullDayEmpHours * EMP_RATE_PER_HOUR
+    return "Employee is full day present and employee wage is",empWage
+
+
+def empHalfDayWage():
+    """
+        Description:
+            Calculating employee half day wage
+        Parameter:
+            None
+        Return:
+            Total employee wage and presenty message.
+    """
+    empWage = halfDayEmpHours * EMP_RATE_PER_HOUR
+    return "Employee is half day present and employee wage is",empWage
+
+
+def empPresentyCheck(presentyCheck):
+    """
+        Description:
+             check employee is present or absent.
+        Parameter:
+             Two random number for employee attendance
+        Return:
+            returning the check variable
+    """
+    switcher = {
+        0: 'Employee is Absent',
+        1: empwageCalculate(random.randint(0, 1))
+    }
+    return switcher[presentyCheck]
+
+
+def empwageCalculate(randomCheck):
+    """
+        Description:
+            calculating employees wage
+        Parameter:
+             two random number for checking full day or half day wage.
+        Return:
+            return function values from full or half day
+    """
+    switcher = {
+        0: empHalfDayWage(),
+        1: empFullDayWage()
+    }
+    return switcher[randomCheck]
+
+empCheck = random.randint(0, 1)
+employeeWage = empPresentyCheck(empCheck)
+
+if empCheck == 0:
+    print(employeeWage)
 else:
-    print("Employee is absent")
+    print(employeeWage[0])
+    print(employeeWage[1])
+
+
