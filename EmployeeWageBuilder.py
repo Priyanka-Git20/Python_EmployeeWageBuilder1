b8a9283bde3e13b811c8c16d506a18064af1bfc4
priyanka <priyanka.chavan1408@gmail.com>
2022-04-04 18:08:12 +0530

    [Priyanka]Added:Calculated Wages till a condition of total working hours or days is reached for a month.

--- EmployeeWageBuilder.py
+++ EmployeeWageBuilder.py
@@ -1,9 +1,9 @@
 '''
 @Author : Priyanka
-@Date : 2022-04-04  17:40:00
+@Date : 2022-04-04  18:05:00
 @Last Modified by : Priyanka
-@Last Modified Time : 2022-04-04  18:00:00
-@Title : Calculate employee wage for a month.
+@Last Modified Time : 2022-04-04  18:25:00
+@Title : Calculate Wages till a condition of total working hours or days is reached for a month.
 '''
 
 import random
@@ -14,8 +14,11 @@ fullDayEmpHours = 8
 halfDayEmpHours = 4
 EMP_RATE_PER_HOUR = 20
 NUM_OF_WORKING_DAYS = 20
-totalEmpWage = 0
+UM_OF_WORKING_DAYS = 20
+MAX_HOURS_IN_MONTH = 100
 totalWorkingDays= 0
+totalEmpHours= 0
+totalEmpWage = 0
 
 
 def empFullDayWage():
@@ -28,7 +31,7 @@ def empFullDayWage():
             Total employee wage and presenty message.
     """
     empWage = fullDayEmpHours * EMP_RATE_PER_HOUR
-    return "Employee is full day present and employee wage is",empWage
+    return "Employee is full day present and employee wage is",empWage,fullDayEmpHours
 
 
 def empHalfDayWage():
@@ -41,7 +44,7 @@ def empHalfDayWage():
             Total employee wage and presenty message.
     """
     empWage = halfDayEmpHours * EMP_RATE_PER_HOUR
-    return "Employee is half day present and employee wage is",empWage
+    return "Employee is half day present and employee wage is",empWage,halfDayEmpHours
 
 
 def empPresentyCheck(presentyCheck):
@@ -75,15 +78,20 @@ def empwageCalculate(randomCheck):
     }
     return switcher[randomCheck]
 
-while(totalWorkingDays < NUM_OF_WORKING_DAYS ):
+
+while (totalEmpHours < MAX_HOURS_IN_MONTH and totalWorkingDays < NUM_OF_WORKING_DAYS):
+    # totalWorkingDays += 1
     empCheck = random.randint(0, 1)
     employeeWage = empPresentyCheck(empCheck)
     if empCheck == 0:
-        (employeeWage)
+        employeeWage
     else:
-        (employeeWage[0])
-        (employeeWage[1])
+        employeeWage[0]
+        employeeWage[1]
         totalEmpWage += employeeWage[1]
+        totalEmpHours += employeeWage[2]
         totalWorkingDays += 1
 
 print("Total employee wage for {} days is {}".format(totalWorkingDays,totalEmpWage))
+print("Total employee hours in {} days are {}".format(totalWorkingDays,totalEmpHours))
+print("Total working days are",totalWorkingDays)
